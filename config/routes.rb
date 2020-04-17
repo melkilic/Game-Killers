69e4025eb37a1cf45e14490ff0cb357aebad63e9
RafaS92 <manrvaldez..92@gmail.com>
2020-04-17 18:09:58 -0500

    semifinal

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,10 @@ Rails.application.routes.draw do
   get("/games/new",{to:'games#new'})
   get("/games/pick",{to:'games#pick_game'})
   get("/games/pick/:id",{to:'games#start_game'})
-<<<<<<< HEAD
-  post('/games/result/:id',{to:'games#create'})
-  # post('/games/result/:id',{to:'application#result'})
-  get("/games/result",{to:'games#result'})
-  
-=======
   
   get('/games/result/:id',{to:'games#result'})
   post('/games/result',{to:'games#correct_answers'})
   get("/games/questions",{to:'games#questions'})
->>>>>>> aa09e64fdd8b3add79e3da25bec37474563d60f1
   get("/log_out",{to:"users#log_out"})
 
 
